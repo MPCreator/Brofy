6a08cd8612b477c0bd43c390b94da38deb08280c
Jorge Ramírez <jorge.ramirezgarciap@gmail.com>
2026-05-19 21:18:14 -0500

    Fix 1.0

--- app/(protected)/checkin/[establishmentId]/page.tsx
+++ app/(protected)/checkin/[establishmentId]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+export const dynamic = 'force-dynamic'
 import { useState, useEffect } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import { getEstablishmentByQr, getUserPets, createAppointment, processPayment } from '@/lib/actions'
